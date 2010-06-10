0558c812165d9d4d53c84f57b676b2a6f9321652
murtazajafferji <murtazajafferji@gmail.com>
2010-06-10 14:43:40 -0400

    added votes

--- app/models/definition.rb
+++ app/models/definition.rb
@@ -3,4 +3,6 @@ class Definition < ActiveRecord::Base
   has_many :moods, :through => :votes
   belongs_to :word
   acts_as_commentable
+  acts_as_voteable :vote_counter => true # Stores the sum of the votes in the `vote_total`
+                                         # column of the `posts` table.
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100609190335) do
+ActiveRecord::Schema.define(:version => 20100610182453) do
 
   create_table "definitions", :force => true do |t|
     t.string   "body"
@@ -32,12 +32,17 @@ ActiveRecord::Schema.define(:version => 20100609190335) do
   end
 
   create_table "votes", :force => true do |t|
-    t.integer  "direction"
+    t.integer  "vote",          :null => false
+    t.integer  "voteable_id",   :null => false
+    t.string   "voteable_type", :null => false
+    t.integer  "voter_id"
+    t.string   "voter_type"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "mood_id"
-    t.integer  "definition_id"
   end
+
+  add_index "votes", ["voteable_id", "voteable_type"], :name => "fk_voteables"
+  add_index "votes", ["voter_id", "voter_type"], :name => "fk_voters"
 
   create_table "words", :force => true do |t|
     t.string   "name"
