09b94a235024d1f4a3845655862c121451b50581
murtazajafferji <murtazajafferji@gmail.com>
2010-06-28 19:08:11 -0400

    added hpricot to .gems

--- config/environment.rb
+++ config/environment.rb
@@ -43,6 +43,7 @@ Rails::Initializer.run do |config|
   config.gem "authlogic-oid", :lib => "authlogic_openid"
   config.gem "ruby-openid", :lib => "openid"
   
+  config.gem "ferret"
   config.gem "acts_as_ferret"
   config.gem 'jackdempsey-acts_as_commentable', :lib => 'acts_as_commentable', :source => "http://gems.github.com"
 
