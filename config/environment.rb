848182c283da1ca6b592eb29f0a38e84079485fc
murtazajafferji <murtazajafferji@gmail.com>
2010-07-09 06:27:43 -0400

    Facebook Connect

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,6 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
-require 'restful_authentication'
-
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
