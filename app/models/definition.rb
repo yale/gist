83e87862ca24fd10019a8d0740861151b4f91b88
Yale Spector <yale@brandeis.edu>
2010-07-13 14:07:29 -0400

    fixing sql

--- app/models/definition.rb
+++ app/models/definition.rb
@@ -70,7 +70,7 @@ class Definition < ActiveRecord::Base
   end
   
   def self.find_popular
-    result = self.find_by_sql("SELECT definitions.*, count(user_votes.id) AS votes FROM definitions, user_votes WHERE user_votes.definition_id = definitions.id AND user_votes.like = 't' AND user_votes.created_at > '#{1.day.ago.strftime("%Y-%m-%d %H:%M:%S")}' GROUP BY definitions.id, definitions.body ORDER BY votes;")
+    result = self.find_by_sql("SELECT definitions.*, count(user_votes.id) AS votes FROM definitions, user_votes WHERE user_votes.definition_id = definitions.id AND user_votes.like = 't' AND user_votes.created_at > '#{1.day.ago.strftime("%Y-%m-%d %H:%M:%S")}' GROUP BY definitions.id, definitions.body, definitions.created_at, definitions.updated_at, definitions.word_id, definitions.like, definitions.dislike, definitions.helpful, definitions.funny, definitions.poetic, definitions.inaccurate, definitions.mature, definitions.offensive, definitions.duplicate, definitions.user_id, definitions.part_of_speech, definitions.category ORDER BY votes;") 
     return result
   end
 end
