ed670ecd48e92443573df414d976db0f8f47f3bb
Yale Spector <yale@brandeis.edu>
2010-07-15 21:09:42 -0400

    fixed timespan for realz

--- app/models/definition.rb
+++ app/models/definition.rb
@@ -76,7 +76,7 @@ class Definition < ActiveRecord::Base
   def self.find_popular timespan = nil
     query = "SELECT definitions.*, count(user_votes.id) AS votes FROM definitions, user_votes WHERE user_votes.definition_id = definitions.id AND user_votes.like = 't'"
     if timespan
-      query += " AND user_votes.created_at > '#{timespan.days.ago.strftime("%Y-%m-%d %H:%M:%S")}'"
+      query += " AND user_votes.created_at > '#{timespan.to_i.days.ago.strftime("%Y-%m-%d %H:%M:%S")}'"
     end
     query += " GROUP BY definitions.id, definitions.body, definitions.created_at, definitions.updated_at, definitions.word_id, definitions.like, definitions.dislike, definitions.helpful, definitions.funny, definitions.poetic, definitions.inaccurate, definitions.mature, definitions.offensive, definitions.duplicate, definitions.user_id, definitions.part_of_speech, definitions.category ORDER BY votes DESC;"
     return self.find_by_sql(query)
