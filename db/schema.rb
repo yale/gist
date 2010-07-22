c1665bb140035e6a8938d9baea05711ba1afd277
murtazajafferji <murtazajafferji@gmail.com>
2010-07-22 01:29:23 -0400

    fixed point problem

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,13 @@
 ActiveRecord::Schema.define(:version => 20100716041870) do
 
   create_table "comments", :force => true do |t|
-    t.string    "title",            :limit => 50, :default => ""
-    t.text      "comment",                        :default => ""
-    t.integer   "commentable_id"
-    t.string    "commentable_type"
-    t.integer   "user_id"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "title",            :limit => 50, :default => ""
+    t.text     "comment",                        :default => ""
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "comments", ["commentable_id"], :name => "index_comments_on_commentable_id"
@@ -26,63 +26,56 @@ ActiveRecord::Schema.define(:version => 20100716041870) do
   add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
 
   create_table "definitions", :force => true do |t|
-    t.string    "body"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.integer   "word_id"
-    t.integer   "like",           :default => 0
-    t.integer   "dislike",        :default => 0
-    t.integer   "helpful",        :default => 0
-    t.integer   "funny",          :default => 0
-    t.integer   "poetic",         :default => 0
-    t.integer   "inaccurate",     :default => 0
-    t.integer   "mature",         :default => 0
-    t.integer   "offensive",      :default => 0
-    t.integer   "duplicate",      :default => 0
-    t.integer   "user_id"
-    t.string    "part_of_speech"
-    t.string    "category"
+    t.string   "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "word_id"
+    t.integer  "like",           :default => 0
+    t.integer  "dislike",        :default => 0
+    t.integer  "helpful",        :default => 0
+    t.integer  "funny",          :default => 0
+    t.integer  "poetic",         :default => 0
+    t.integer  "inaccurate",     :default => 0
+    t.integer  "mature",         :default => 0
+    t.integer  "offensive",      :default => 0
+    t.integer  "duplicate",      :default => 0
+    t.integer  "user_id"
+    t.string   "part_of_speech"
+    t.string   "category"
   end
 
   create_table "sessions", :force => true do |t|
-    t.string    "session_id", :null => false
-    t.text      "data"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "trigrams", :force => true do |t|
-    t.string    "tg"
-    t.integer   "word_id"
-    t.integer   "score"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-  end
-
-  create_table "user_sessions", :force => true do |t|
-    t.string    "username"
-    t.string    "password"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "tg"
+    t.integer  "word_id"
+    t.integer  "score"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "user_votes", :force => true do |t|
-    t.boolean   "like"
-    t.boolean   "dislike"
-    t.boolean   "helpful"
-    t.boolean   "funny"
-    t.boolean   "poetic"
-    t.boolean   "inaccurate"
-    t.boolean   "mature"
-    t.boolean   "offensive"
-    t.boolean   "duplicate"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.integer   "user_id"
-    t.integer   "definition_id"
+    t.boolean  "like"
+    t.boolean  "dislike"
+    t.boolean  "helpful"
+    t.boolean  "funny"
+    t.boolean  "poetic"
+    t.boolean  "inaccurate"
+    t.boolean  "mature"
+    t.boolean  "offensive"
+    t.boolean  "duplicate"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+    t.integer  "definition_id"
   end
 
   create_table "users", :force => true do |t|
@@ -97,23 +90,23 @@ ActiveRecord::Schema.define(:version => 20100716041870) do
     t.datetime "remember_token_expires_at"
     t.integer  "fb_user_id"
     t.string   "email_hash"
-    t.string   "url"
+    t.string   "url",                       :limit => 100
     t.integer  "points",                                   :default => 0
   end
 
   create_table "votes", :force => true do |t|
-    t.integer   "direction"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.integer   "mood_id"
-    t.integer   "definition_id"
+    t.integer  "direction"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "mood_id"
+    t.integer  "definition_id"
   end
 
   create_table "words", :force => true do |t|
-    t.string    "name"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.string    "url"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "url",        :limit => nil
   end
 
 end
