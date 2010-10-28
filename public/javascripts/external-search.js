cb5cbc7f6dcf31a23ba0ecd59afba8d2f9536edd
Murtaza Jafferji <murtazajafferji@gmail.com>
2010-10-27 22:03:57 -0500

    added expand and contract buttons to iframes

--- public/javascripts/external-search.js
+++ public/javascripts/external-search.js
@@ -6,6 +6,7 @@ function search(word, url, identifier) {
 function showIframe(identifier) {
     document.getElementById("searchResult" + identifier).style.display = "";
     document.getElementById("toggleButton" + identifier).style.display = "";
+	document.getElementById("sizeButton" + identifier).style.display = "";
 	document.getElementById("searchButton" + identifier).style.display = "none";
 }
 
@@ -19,6 +20,20 @@ function toggleIframe(identifier) {
 		value = "Show"
     }
     document.getElementById("searchResult" + identifier).style.display = display;
+	document.getElementById("sizeButton" + identifier).style.display = display;
 	document.getElementById("toggleButton" + identifier).value = value;
 	
+}
+
+function changeSize(identifier) {
+	var size = document.getElementById("searchResult" + identifier).style.width;
+	if (size == "500px") {
+	    document.getElementById("searchResult" + identifier).style.width = "575px";
+		document.getElementById("searchResult" + identifier).style.height = "600px";
+		document.getElementById("sizeButton" + identifier).value = "Contract";
+    } else {
+		document.getElementById("searchResult" + identifier).style.width = "500px";
+		document.getElementById("searchResult" + identifier).style.height = "200px";
+		document.getElementById("sizeButton" + identifier).value = "Expand";
+    }
 }
