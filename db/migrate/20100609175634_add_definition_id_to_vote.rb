c82ead744a6b6768c12d889d3010f1605aba0142
Murtaza Jafferji <murtazajafferji@gmail.com>
2010-11-12 18:19:21 -0600

    fix mail gem plugin problem

--- db/migrate/20100609175634_add_definition_id_to_vote.rb
+++
@@ -1,9 +0,0 @@
-class AddDefinitionIdToVote < ActiveRecord::Migration
-  def self.up
-    add_column :votes, :definition_id, :integer
-  end
-
-  def self.down
-    remove_column :votes, :definition_id
-  end
-end
