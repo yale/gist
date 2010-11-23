9314b0012608e18d327ed67266bf8a0ce8c2a038
Murtaza Jafferji <murtazajafferji@gmail.com>
2010-11-23 16:17:53 -0600

    removed files

--- public/404_files/likebox_data/a_data/HWL11JhzW9w.js
+++
@@ -1,7 +0,0 @@
-/*
-HTTP Host: static.ak.fbcdn.net
-Generated: September 27th 2010 10:54:59 AM PDT
-Machine: 10.30.146.198
-*/
-
-((location=='about:blank'&&(window.parent.eval_global||window.parent.eval))||(window.eval_global||window.eval))("if (window.CavalryLogger) { CavalryLogger.start_js([\"js\\\/detect_broken_proxy_cache.js\"]); }\n\nfunction detect_broken_proxy_cache(d,a){var b=getCookie(a);if((b!=d)&&(b!=null)&&(d!='0')){var c={c:'si_detect_broken_proxy_cache',m:a+' '+d+' '+b};var e=new URI('\/common\/scribe_endpoint.php').getQualifiedURI().toString();new AsyncSignal(e,c).send();}}\n\nif (window.Bootloader) { Bootloader.done([\"js\\\/detect_broken_proxy_cache.js\"]); }")
--- public/javascripts/views/words.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-  $("input#word_name").autocomplete({ ajax: "auto_complete_for_word_name" })
-});
