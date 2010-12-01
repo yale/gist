8690cf24ecee2f703819d320a7e55da4fba1f665
Murtaza Jafferji <murtazajafferji@gmail.com>
2010-11-30 23:51:37 -0600

    increased maxwidth for embedded links

--- app/models/definition.rb
+++ app/models/definition.rb
@@ -392,7 +392,7 @@ class Definition < ActiveRecord::Base
 	  #OEmbed::Providers.register_all unless links.empty?
 	  OEmbed::Providers.register(OEmbed::Providers::Embedly) unless links.empty?
 	  links.each do |l|
-	    res = OEmbed::Providers.get(l, {:maxwidth => 300, :maxheight => 300}) rescue OEmbed::Error
+	    res = OEmbed::Providers.get(l, {:maxwidth => 500, :maxheight => 300}) rescue OEmbed::Error
 	    embedded_links.push res.html rescue OEmbed::Error
     end
     embedded_links
